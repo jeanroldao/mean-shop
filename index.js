dd139d4ba7eb23c9cdf9aaa6664d6c3ca238c5f1
Jean Farias Roldão <jeanroldao@gmail.com>
2016-01-17 23:47:40 -0200

    Enable nodejs express cross domain requests
    Signed-off-by: Jean Farias Roldão <jeanroldao@gmail.com>

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ require('./dependencies')(wagner);
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.append('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.append('Access-Control-Allow-Credentials', 'true');
+  res.append('Access-Control-Allow-Methods', ['GET', 'OPTIONS', 'PUT', 'POST']);
+  res.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 wagner.invoke(require('./auth'), { app: app });
 
 app.use('/api/v1', require('./api')(wagner));
 
-app.use(express.static('.'));
+app.use(express.static('static'));
 
 app.listen(3000);
 console.log('Server listening on port 3000');
